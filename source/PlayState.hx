696f970ffa588119323992e4d12dc2fbf6f89258
tfxdx <yunumendozaperez@gmail.com>
2026-04-19 16:30:30 -0500

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,7 +3,6 @@ package;
 import openfl.geom.Matrix;
 import openfl.display.BitmapData;
 import openfl.utils.AssetType;
-import openfl.utils.Assets as OpenFlAssets;
 import lime.graphics.Image;
 import flixel.graphics.FlxGraphic;
 import openfl.utils.AssetManifest;
@@ -416,9 +415,7 @@ class PlayState extends MusicBeatState
 	function makeLuaSprite(spritePath:String,toBeCalled:String, drawBehind:Bool)
 	{
 		#if sys
-		var pathData:String = "assets/data/" + PlayState.SONG.song.toLowerCase() + '/' + spritePath + ".png";
-
-		var data:BitmapData = OpenFlAssets.getBitmapData(pathData);
+		var data:BitmapData = BitmapData.fromFile(Sys.getCwd() + "assets/data/" + PlayState.SONG.song.toLowerCase() + '/' + spritePath + ".png");
 
 		var sprite:FlxSprite = new FlxSprite(0,0);
 		var imgWidth:Float = FlxG.width / data.width;
@@ -478,15 +475,14 @@ class PlayState extends MusicBeatState
 		repPresses = 0;
 		repReleases = 0;
 
-		var modchartPath:String = "assets/data/" + PlayState.SONG.song.toLowerCase() + "/modchart.lua";
-
-		executeModchart = OpenFlAssets.exists(modchartPath);
-
+		#if sys
+		executeModchart = FileSystem.exists(Paths.lua(PlayState.SONG.song.toLowerCase()  + "/modchart"));
+		#end
 		#if !cpp
 		executeModchart = false; // FORCE disable for non cpp targets //Hey, wtf is 'cpp targets'? -Haz
 		#end
 
-		trace('Mod chart: ' + executeModchart + " - " + modchartPath);
+		trace('Mod chart: ' + executeModchart + " - " + Paths.lua(PlayState.SONG.song.toLowerCase() + "/modchart"));
 
 		#if windows
 		// Making difficulty text for Discord Rich Presence.
@@ -2593,6 +2589,26 @@ class PlayState extends MusicBeatState
 
 		var playerCounter:Int = 0;
 
+		// Per song offset check
+		#if cpp
+			var songPath = 'assets/data/' + PlayState.SONG.song.toLowerCase() + '/';
+			for(file in sys.FileSystem.readDirectory(songPath))
+			{
+				var path = haxe.io.Path.join([songPath, file]);
+				if(!sys.FileSystem.isDirectory(path))
+				{
+					if(path.endsWith('.offset'))
+					{
+						trace('Found offset file: ' + path);
+						songOffset = Std.parseFloat(file.substring(0, file.indexOf('.off')));
+						break;
+					}else {
+						trace('Offset file not found. Creating one @: ' + songPath);
+						sys.io.File.saveContent(songPath + songOffset + '.offset', '');
+					}
+				}
+			}
+		#end
 		var daBeats:Int = 0; // Not exactly representative of 'daBeats' lol, just how much it has looped
 		for (section in noteData)
 		{
